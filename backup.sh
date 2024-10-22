b33097793146a54496da46f920396088a36d719a
zildCh <93721435+zildCh@users.noreply.github.com>
2024-10-22 16:49:46 +0700

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,7 +4,7 @@
 DB_PATH="/app/data/preRecordingDatabase.db"
 BACKUP_PATH="/app/data/PREdatabase_backup_$(date +\%Y-\%m-\%d).db"
 SFTP_USER="Sasha"
-SFTP_PASS="Cfif_x005"
+SFTP_PASS="******"
 SFTP_HOST="5.130.130.194"
 SFTP_PORT="22"
 SFTP_DEST_DIR="/tmp/mnt/7898866E98862B28/preRecordingBackup"
